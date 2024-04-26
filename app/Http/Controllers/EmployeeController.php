db7b5ed05966b81174e7c4c2c8ffcad7fd4f9794
HelloSunnah <shahriasunnah0@gmail.com>
2024-04-26 23:55:02 +0600

    new

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -2,110 +2,83 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Employee;
 use Illuminate\Http\Request;
 
 class EmployeeController extends Controller
 {
-
-    public function employee_create()
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
     {
-        return view('page.EmployeeCreate');
+        //
     }
 
-
-
-
-
-    public function employee_create_post(Request $request)
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
     {
-
-        $fileName = null;
-        
-        if ($request->hasFile('image'))
-
-         {
-            $fileName = time() . '.' . $request->file('image')->getclientOriginalExtension();
-            $request->file('image')->move(public_path('/uploads/image'), $fileName);
-        } 
-
-      
-        Employee::create([
-
-            'name' => $request->name,
-            'image' => $fileName,
-            'designation' => $request->designation,
-            'age' => $request->age,
-            'mobile' => $request->mobile,
-            'par_address' => $request->par_address,
-            'pre_address' => $request->pre_address,
-            'email' => $request->email,
-            'employee_id' => $request->employee_id,
-            'salary' => $request->salary,
-            'nid' => $request->nid,
-            
-        ]);
-        toastr()->addSuccess('Employee Created.');
-
-        return  back();
+        //
     }
 
-    public function employee_list()
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
     {
-        $employee = Employee::all();
-        return view('page.EmployeeList', compact('employee'));
+        //
     }
 
-
-
-    public function employee_edit($id)
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
     {
-        $employee = Employee::find($id);
-
-
-        return view('page.EmployeeEdit', compact('employee'));
+        //
     }
-    public function employee_update(Request $request, $id)
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
     {
-        $employee = Employee::find($id);
-
-        $fileName = null;
-        if ($request->hasFile('image')) {
-            $fileName = time() . '.' . $request->file('image')->getclientOriginalExtension();
-            $request->file('image')->move(public_path('/uploads/image'), $fileName);
-        } 
-        $employee->update([
-
-            'name' => $request->name,
-            'designation' => $request->designation,
-            'age' => $request->age,
-            'par_address' => $request->par_address,
-            'pre_address' => $request->pre_address,
-            'email' => $request->email,
-            'employee_id' => $request->employee_id,
-            'salary' => $request->salary,
-            'nid' => $request->nid,
-            'image'=>$fileName,
-
-
-
-        ]);
-        toastr()->addSuccess('Employee Details Updated.');
-
-        return back();
+        //
     }
-    public function  employee_delete($id)
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
     {
-        $employee = Employee::find($id);
-        $employee->delete();
-
-        toastr()->addSuccess('Opps Employee Deleted.');
-
-        return back();
+        //
     }
-    public function employee_view($id)
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
     {
-        $employee = Employee::find($id);
-        return view('page.EmployeeView', compact('employee'));
+        //
     }
 }
